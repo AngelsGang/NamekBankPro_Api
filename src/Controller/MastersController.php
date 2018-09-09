ee45bc150e0a9ab30472099ffbc081e22900252a
AngelsGang <yeezy.ag@gmail.com>
2018-09-09 22:21:59 +0200

    update du MasterController

--- src/Controller/MastersController.php
+++ src/Controller/MastersController.php
@@ -4,14 +4,27 @@ namespace App\Controller;
 
 use App\Entity\Master;
 use App\Repository\MasterRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MastersController extends FOSRestController
 {
+    private $masterRepository;
+    private $em;
+
+    public function __construct(MasterRepository $masterRepository, EntityManagerInterface $em)
+    {
+        $this->masterRepository = $masterRepository;
+        $this->em = $em;
+    }
+
     public function getUsersAction()
-    {}
+    {
+        $masters = $this->masterRepository->findAll();
+        return $this->view($masters);
+    }
     public function getUserAction()
     {}
     public function postUsersAction()
